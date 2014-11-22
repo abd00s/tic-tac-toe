c03c1e3ce33f6d9ff00d4ac7375f78d9aae6a06d
Abdullah <abd00s@users.noreply.github.com>
2014-11-21 20:50:57 -0500

    clean indentation, add tie scenario

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
 
-  // Add marker to clicked position; alternate X's and O's
-
   pOneSelections = [];
   pTwoSelections = [];
+
   var turn = 0;
   $('td').on("click", function(){
     if (turn % 2 == 0) {
@@ -11,94 +10,81 @@ $(document).ready(function(){
       $self.text('X');
       pOneSelections.push(parseInt($self.attr('id')));
       if (pOneSelections.length >= 3){
-
-        // Winning through first 3 selections
-
-        if (pOneSelections.length == 3){
-          var total = 0;
-          for (var i = 0; i < pOneSelections.length; i++) {
-            // console.log(total);
-            if (total == 15){
-              alert("P1 wins");
-            }else{
-              $('h2').text("Player Two GO!");
-            }
-          }
-            total += pOneSelections[i] << 0; // Observe!
-
-        // Winning on fourth selection  
-        
-      }else if (pOneSelections.length == 4){
-
-        if (pOneSelections[0] + pOneSelections[1] + pOneSelections[3] == 15) {
-          alert("P1 wins");
-
-        }else if (pOneSelections[0] + pOneSelections[2] + pOneSelections[3] == 15) {
-          alert("P1 wins");
-
-        }else if (pOneSelections[1] + pOneSelections[2] + pOneSelections[3] == 15) {
-          alert("P1 wins");
-        }else{
-          $('h2').text("Player Two GO!");
+        var total = 0;
+        for (var i = 0; i < pOneSelections.length; i++) {
+          total += pOneSelections[i] << 0;
         }
         
-        // Winning on 5th selection
+        if (pOneSelections.length == 3){
+          if (total == 15){
+            alert("P1 wins");
+          }else{
+            $('h2').text("Player Two GO!");
+          }
+        }else if (pOneSelections.length == 4){
+          if (pOneSelections[0] + pOneSelections[1] + pOneSelections[3] == 15) {
+            alert("P1 wins");
 
-      }else{
+          }else if (pOneSelections[0] + pOneSelections[2] + pOneSelections[3] == 15) {
+            alert("P1 wins");
 
-        if (pOneSelections[0] + pOneSelections[3] + pOneSelections[4] == 15){
-          alert("P1 wins");
+          }else if (pOneSelections[1] + pOneSelections[2] + pOneSelections[3] == 15) {
+            alert("P1 wins");
+          }else{
+            $('h2').text("Player Two GO!");
+          }
+        }else{
+          if (pOneSelections[0] + pOneSelections[3] + pOneSelections[4] == 15){
+            alert("P1 wins");
 
-        }else if(pOneSelections[1] + pOneSelections[3] + pOneSelections[4] == 15){
-          alert("P1 wins");
+          }else if(pOneSelections[1] + pOneSelections[3] + pOneSelections[4] == 15){
+            alert("P1 wins");
 
-        }else if (pOneSelections[2] + pOneSelections[3] + pOneSelections[4] == 15){
-          alert("P1 wins");
+          }else if (pOneSelections[2] + pOneSelections[3] + pOneSelections[4] == 15){
+            alert("P1 wins");
 
-        }else if(pOneSelections[0] + pOneSelections[1] + pOneSelections[4] == 15){
-          alert("P1 wins");
+          }else if(pOneSelections[0] + pOneSelections[1] + pOneSelections[4] == 15){
+            alert("P1 wins");
 
-        }else if(pOneSelections[0] + pOneSelections[2] + pOneSelections[4] == 15){
-          alert("P1 wins");
+          }else if(pOneSelections[0] + pOneSelections[2] + pOneSelections[4] == 15){
+            alert("P1 wins");
+          }else{
+            alert("Tie Game!");
+          }
         }
       }
-
-
     }else{
-      $('h2').text("Player Two GO!");
-    }
-  }else{
-    var $self = $(this)
-    $self.text('O');
-    pTwoSelections.push(parseInt($self.attr('id')));
-    $('h2').text("Player One GO!");
-    if (pTwoSelections.length >= 3){
-      if (pTwoSelections.length == 3){
+      //p2 magic
+      var $self = $(this)
+      $self.text('O');
+      pTwoSelections.push(parseInt($self.attr('id')));
+      if (pTwoSelections.length >= 3){
         var total = 0;
         for (var i = 0; i < pTwoSelections.length; i++) {
+          total += pTwoSelections[i] << 0;
+        }
+        if (pTwoSelections.length == 3){
           if (total == 15){
             alert("P2 wins");
           }else{
             $('h2').text("Player One GO!");
           }
-        }        
-        total += pTwoSelections[i] << 0; // Observe!
-      }else{
-        if (pTwoSelections[0] + pTwoSelections[1] + pTwoSelections[3] == 15) {
-          alert("P2 wins");
-
-        }else if (pTwoSelections[0] + pTwoSelections[2] + pTwoSelections[3] == 15) {
-          alert("P2 wins");
-
-        }else if (pTwoSelections[1] + pTwoSelections[2] + pTwoSelections[3] == 15) {
-          alert("P2 wins");
         }else{
-          $('h2').text("Player Two GO!");
-        } 
+          if (pTwoSelections[0] + pTwoSelections[1] + pTwoSelections[3] == 15) {
+            alert("P2 wins");
+
+          }else if (pTwoSelections[0] + pTwoSelections[2] + pTwoSelections[3] == 15) {
+            alert("P2 wins");
+
+          }else if (pTwoSelections[1] + pTwoSelections[2] + pTwoSelections[3] == 15) {
+            alert("P2 wins");
+          }else{
+            $('h2').text("Player Two GO!");
+          } 
+        }
       }
     }
-  }
-  turn++; 
-  $(this).off("click")
-});
+    turn ++;
+    $(this).off("click")
+  });
 });
